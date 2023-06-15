f4ef2e92580b7dd1b1646ec3f4916d834c0a9f69
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-16 03:50:43 +0400

    Create navigation, add logo and Google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Container from "./components/lib/Container/container";
+import React from "react";
+import Container from "./components/lib/Container/Container";
+import Navigation from "./constants/navigation";
 
 const App = () => {
   return (
     <>
       <Container>
+        <Navigation />
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </Container>
     </>
