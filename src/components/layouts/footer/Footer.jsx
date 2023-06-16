0b8aa0d2ca5cde76a344de5fda68a4fb16cb4297
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-17 01:59:20 +0400

    Add more CSS for footer and make it responsive for devices

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,86 +1,98 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 import logo from "../../../assets/logo-grey.png";
-import { textColor } from "../../../constant/colors";
+import { primaryColor } from "../../../constant/colors";
 
 const Footer = () => {
+  const socialMediaPlatforms = [
+    { icon: faFacebook, link: "https://www.facebook.com" },
+    { icon: faInstagram, link: "https://www.instagram.com" },
+    { icon: faTwitter, link: "https://www.twitter.com" },
+  ];
+
   return (
-    <footer
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        padding: "1rem",
-        color: textColor,
-      }}
-    >
-      <div>
-        <img
-          src={logo}
-          alt="Logo"
-          style={{ height: "90px" }}
-          className="font-extrabold"
-        />
-      </div>
-      <div>
-        <ul>
-          <Link to="/about" style={{ marginRight: "1rem" }}>
-            About Us
-          </Link>
-          <li
-            style={{ width: "350px", fontWeight: 200 }}
-            className="text-sm pt-8 leading-5"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo
-            amet posuere porta vitae mi accumsan ultricies. Commodo amet posuere
-            porta vitae mi accumsan ultricies.
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <Link to="/contact">Contact Us</Link>
-          <div className="text-sm" style={{ fontWeight: 200 }}>
-            <li className="pt-8">Address: </li>
-            <li className="pt-3">Phone: </li>
-            <li className="pt-3">E-mail: </li>
-          </div>
-        </ul>
-      </div>
-      <div>
-        <ul style={{ marginRight: "1rem" }}>
-          Subscribe
-          <form className="max-w-sm mx-auto flex pt-3">
-            <input
-              className="shadow appearance-none border rounded-l w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="email"
-              placeholder="Your Email"
-            />
-            <button
-              className="text-black font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline"
-              type="submit"
+    <footer className="py-8 text-black md:px-4 lg:px-24">
+      <div className="flex flex-col items-center md:flex-row md:justify-between">
+        <div>
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ height: "90px" }}
+            className="font-extrabold"
+          />
+        </div>
+        <div className="mt-4 md:mt-0">
+          <ul className="text-sm text-center md:text-left">
+            <li className="mb-3">
+              <Link to="/about" className="text-black">
+                About Us
+              </Link>
+            </li>
+            <li
+              className="text-black leading-6 w-64"
+              style={{ fontWeight: 200 }}
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 5l7 7-7 7"
-                ></path>
-              </svg>
-            </button>
-          </form>
-          <div>
-            <ul className="pt-4">
-              Follow Us
-              <li>Facebook Instagram Twitter</li>
-            </ul>
-          </div>
-        </ul>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo
+              amet posuere porta vitae mi accumsan ultricies.
+            </li>
+          </ul>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <ul className="text-sm text-center md:text-left">
+            <li className="mb-3">
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <div className="leading-6" style={{ fontWeight: 200 }}>
+              <li className="mb-2">Address:</li>
+              <li className="mb-2">Phone:</li>
+              <li>E-mail:</li>
+            </div>
+          </ul>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <ul className="text-sm text-center md:text-left">
+            <li className="mb-3">Subscribe</li>
+            <form className="flex">
+              <input
+                className={`border rounded-lg py-1 px-3 text-gray-300 text-xs bg-${primaryColor} md:w-48 md:mr-1`}
+                type="email"
+                placeholder="Your Email"
+              />
+              <button className="text-black py-1 px-2" type="submit">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path strokeWidth="3" d="M9 5l7 7-7 7"></path>
+                </svg>
+              </button>
+            </form>
+            <div className="pt-4">
+              <p className="mb-2">Follow Us</p>
+              <div className="flex justify-center md:justify-start">
+                {socialMediaPlatforms.map((platform, index) => (
+                  <a
+                    key={index}
+                    href={platform.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="ml-2"
+                  >
+                    <FontAwesomeIcon icon={platform.icon} size="lg" />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </ul>
+        </div>
       </div>
     </footer>
   );
