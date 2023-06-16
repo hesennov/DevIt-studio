5deceddfa0e6d701394c43f21482e188e2c96344
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-16 20:03:03 +0400

    Add styles folder in src

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,4 +1,46 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo-grey.png";
+import { textColor } from "../../../constant/colors";
+
 const Footer = () => {
-  return <div className="text-6xl p-10 bg-slate-600">Footer</div>;
+  return (
+    <footer
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        padding: "1rem",
+        color: textColor,
+        background: "#DCDCDC",
+      }}
+    >
+      <div>
+        <img src={logo} alt="Logo" style={{ height: "90px" }} />
+      </div>
+      <div>
+        <ul>
+          <Link to="/about" style={{ marginRight: "1rem" }}>
+            About Us
+          </Link>
+          <li style={{ width: "350px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo
+            amet posuere porta vitae mi accumsan ultricies.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <Link to="/contact">Contact Us</Link>
+          <li>Address</li>
+          <li>Phone</li>
+          <li>E-mail</li>
+        </ul>
+      </div>
+      <div>
+        <button style={{ marginRight: "1rem" }}>Subscribe</button>
+      </div>
+    </footer>
+  );
 };
+
 export default Footer;
