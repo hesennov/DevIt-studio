432b7132de26cd5b61e19cba0bdf2d3a442808f6
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-17 12:36:48 +0400

    Add ellipse on the background of the footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,42 @@ import AboutUs from "./pages/AboutUs";
 import Portofilo from "./pages/Portofilo";
 import TestimonIals from "./pages/TestimonIals";
 import Blog from "./pages/Blog";
+import ellipseImage from "./assets/ellipse3.png";
 
 const App = () => {
   return (
     <>
-      <Layout title={"Home"}>
-        <section>
-          <Header />
-        </section>
-        <section className={"mt-20"}>
-          <Industries />
-        </section>
-        <section className={"mt-20"}>
-          <AboutUs />
-        </section>
-        <section className={"mt-20"}>
-          <Portofilo />
-        </section>
-        <section className={"mt-20"}>
-          <TestimonIals />
-        </section>
-        <section className={"mt-20"}>
-          <Blog />
-        </section>
-      </Layout>
+      <div
+        style={{
+          position: "relative", // Add relative positioning to the container
+          paddingBottom: "60px", // Adjust the padding bottom as needed
+          backgroundImage: `url(${ellipseImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "left bottom",
+          backgroundSize: "45%",
+        }}
+      >
+        <Layout title={"Home"}>
+          <section>
+            <Header />
+          </section>
+          <section className={"mt-20"}>
+            <Industries />
+          </section>
+          <section className={"mt-20"}>
+            <AboutUs />
+          </section>
+          <section className={"mt-20"}>
+            <Portofilo />
+          </section>
+          <section className={"mt-20"}>
+            <TestimonIals />
+          </section>
+          <section className={"mt-20"}>
+            <Blog />
+          </section>
+        </Layout>
+      </div>
     </>
   );
 };
