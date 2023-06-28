c8bbcbe477f5c47543264c352ffa953f3806bdac
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-06-29 00:21:35 +0400

    Fix path in BlogPage

--- src/pages/blog/BlogPage.jsx
+++ src/pages/blog/BlogPage.jsx
@@ -1,4 +1,4 @@
-import BlogCard from "../../components/lib/blogCard/BlogCard";
+import BlogCard from "../../components/lib/BlogCard/BlogCard";
 import { headerColor } from "../../constant/colors";
 
 const BlogPage = () => {
