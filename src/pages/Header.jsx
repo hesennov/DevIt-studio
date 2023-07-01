c9914f206b57f4c3a835c73223e9331b6cbb41e1
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-07-01 13:47:31 +0400

    Add Typist to testimonial section

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -12,7 +12,7 @@ function Section({ children }) {
     <section ref={ref}>
       <span
         style={{
-          transform: isInView ? "none" : "translateX(-200px)",
+          transform: isInView ? "none" : "translateX(-100px)",
           opacity: isInView ? 1 : 0,
           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}
