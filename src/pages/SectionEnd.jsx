bd6d11dfd9bb456e1604bd34148c1b8cce1a1274
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-07-01 23:08:03 +0400

    Replace img of Projects with carousel

--- src/pages/SectionEnd.jsx
+++ src/pages/SectionEnd.jsx
@@ -26,7 +26,7 @@ const SectionEnd = () => {
     <div className="relative">
       <Section>
         <img src={banner} alt="" />
-        <button className="absolute top-1/2 md:top-2/4 lg:top-1/2 right-16 md:right-30 lg:right-60 transform -translate-y-1/2 translate-x-1/2 px-4 py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 bg-white rounded-md text-sm md:text-base font-medium shadow-sm hover:shadow-lg text-gray-600 hover:text-indigo-700">
+        <button className="absolute top-1/2 md:top-2/4 lg:top-1/2 right-16 md:right-30 lg:right-60 transform -translate-y-1/2 translate-x-1/2 px-4 py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 bg-white rounded-xl text-sm md:text-base font-medium shadow-sm hover:shadow-lg text-gray-600 hover:text-indigo-700">
           Get in touch
         </button>
       </Section>
