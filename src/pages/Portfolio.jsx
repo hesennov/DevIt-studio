fdbb29c859419927cd5e4883efa89cd853268200
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-07-02 11:28:04 +0400

    Make carousel motion smoother

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -33,7 +33,7 @@ const ProjectPortfolio = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 100,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
